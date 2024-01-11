aaadf06ffa16600d1aeca825a2245ab48650ce3f
G Key <g.key0325@gmail.com>
2024-01-11 15:31:36 -0800

    first

---
+++ ECommerce/App.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type { Node } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
+import Counter from './src/pages/Counter';
+
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    </SafeAreaProvider>
+  )
+}
+
+export default App;
